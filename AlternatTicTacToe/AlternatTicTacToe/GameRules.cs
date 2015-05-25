dd4572553f151cf993572d2e05aac49caf448d10
EvgeniyFes <fes1991@mail.ru>
2015-05-25 21:40:48 +0700

    TicTacToe
    Last version ever

---
+++ AlternatTicTacToe/AlternatTicTacToe/GameRules.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlternatTicTacToe
+{
+    class GameRules : GameField
+    {
+        public GameField[] cell = new GameField[9];
+        public GameField global = new GameField();
+        public bool player1 = true;
+        public int nextCell = -1;
+
+        public int checkForWin(GameField cell)
+        {
+            if ((cell.field[0] != 0) && (cell.field[0] == cell.field[4]) && (cell.field[0] == cell.field[8]))
+                return cell.field[0];
+            else if ((cell.field[2] != 0) && (cell.field[2] == cell.field[4]) && (cell.field[2] == cell.field[6]))
+                return cell.field[2];
+
+            for (int i = 0; i < 9; i += 3)
+            {
+                if ((cell.field[i] != 0) && (cell.field[i + 1] == cell.field[i]) && (cell.field[i] == cell.field[i + 2]))    //vertikal
+                    return cell.field[i];
+                else if ((cell.field[i / 3] != 0) && (cell.field[i / 3 + 3] == cell.field[i / 3]) && (cell.field[i / 3 + 6] == cell.field[i / 3]))   //horizontal
+                    return cell.field[i / 3];
+            }
+            return 0;
+        }
+        public bool checkIfFull(GameField cell)
+        {
+            for (int i = 0; i < 9; ++i)
+            {
+                if (cell.field[i] == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        public bool checkForEnable(GameField cell, int number)
+        {
+            if (cell.field[number] != 0)
+                return false;
+            return true;
+        }
+
+        public bool takeDataForGame()
+        {
+            if (nextCell != -1 && nextCell != -2)
+            {
+                if (checkIfFull(cell[nextCell]))
+                {
+                    nextCell = -1;
+                    Console.WriteLine("Field is full. Choose another field");
+                    return takeDataForGame();
+                }
+            }
+
+            if (nextCell == -1)
+            {
+                nextCell = takeDataForGlobalField();
+                return takeDataForGame();
+            }
+            if (nextCell == -2)
+                return false;
+
+            Console.WriteLine("You in field {0}", nextCell + 1);
+
+
+            Console.Write("Cell number in local game: ");
+            int cellNumber;
+
+            string readValue = Console.ReadLine();
+            if (readValue.Equals("exit"))
+            {
+                return false;
+            }
+
+            bool result = Int32.TryParse(readValue, out cellNumber);
+            if (cellNumber >= 1 && cellNumber <= 9)
+            {
+                --cellNumber;
+                if (checkForEnable(cell[nextCell], cellNumber))
+                {
+                    cell[nextCell].field[cellNumber] = (player1) ? 1 : 2;
+                    nextCell = cellNumber;
+                }
+                else
+                {
+                    Console.WriteLine("Cell busy, choose another cell");
+                    return takeDataForGame();
+                }
+            }
+            else
+            {
+                Console.WriteLine("You input wrong value! Try again:");
+                return takeDataForGame();
+            }
+            return true;
+        }
+
+
+        static int takeDataForGlobalField()
+        {
+            Console.Write("Field number in Global game: ");
+            string readValue = Console.ReadLine();
+
+            if (readValue.Equals("exit"))
+            {
+                return -2;
+            }
+
+            int number;
+
+            bool result = Int32.TryParse(readValue, out number);
+            if (number >= 1 && number <= 9)
+            {
+                return --number;
+            }
+            else
+            {
+                Console.WriteLine("You input wrong value! Try again:");
+                return takeDataForGlobalField();
+            }
+        }
+
+
+        public bool checkForWin()
+        {
+            for (int i = 0; i < 9; ++i)
+            {
+                if ((checkForWin(cell[i]) == 1 || checkForWin(cell[i]) == 2) && global.field[i] == 0)
+                    global.field[i] = (checkForWin(cell[i]) == 1) ? 1 : 2;
+
+                if (checkIfFull(cell[i]) && checkForWin(cell[i]) == 0)
+                    global.field[i] = 3;
+            }
+
+            if (checkForWin(global) != 0)
+            {
+                Console.WriteLine("Player{0} - You Win!!!", checkForWin(global));
+                return false;
+            }
+
+            if (checkForWin(global) == 0 && checkIfFull(global))
+            {
+                Console.WriteLine("Draw!");
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- AlternatTicTacToe/AlternatTicTacToe/Program.cs
+++ AlternatTicTacToe/AlternatTicTacToe/Program.cs
@@ -32,139 +32,33 @@ namespace AlternatTicTacToe
             Console.WriteLine("fields and cells count from 1 to 9");
             Console.WriteLine("to leave the game write \"exit\"\n");
 
-            int nextCell = -1;
-            bool player1 = true;
             bool game = true;
+
+            GameRules rules = new GameRules();
+            rules.cell = cell;
+            rules.global = global;
 
             while (game)
             {
-                playerName(player1);
-                game = takeDataForGame(player1, ref nextCell, global, cell);
-                if (game)
-                    player1 = !player1;
-                else
+                playerName(rules.player1);
+                game = rules.takeDataForGame();
+                rules.player1 = !rules.player1;
+
+                if(!game)
                 {
                     Console.WriteLine("See you later!");
                     return 0;
                 }
-
-                game = checkForWin(cell, global);
-
-                showResultAtConsole(cell, global);
+                game = rules.checkForWin();
+                showResultAtConsole(rules.cell, global);
             }
             return 0;
         }
 
-        static bool takeDataForGame(bool player, ref int nextCell, GameField global, GameField[] cell)
-        {
-            if (nextCell != -1 && nextCell != -2)
-            {
-                if (cell[nextCell].checkIfFull())
-                {
-                    nextCell = -1;
-                    Console.WriteLine("Field is full. Choose another field");
-                    return takeDataForGame(player, ref nextCell, global, cell);
-                }
-            }
-
-            if (nextCell == -1)
-            {
-                nextCell = takeDataForGlobalField();
-                return takeDataForGame(player, ref nextCell, global, cell);
-            }
-            if (nextCell == -2)
-                return false;
-
-            Console.WriteLine("You in field {0}", nextCell + 1);
-
-
-            Console.Write("Cell number in local game: ");
-            int cellNumber;
-
-            string readValue = Console.ReadLine();
-            if (readValue.Equals("exit"))
-            {
-                return false;
-            }
-
-            bool result = Int32.TryParse(readValue, out cellNumber);
-            if (cellNumber >= 1 && cellNumber <= 9)
-            {
-                --cellNumber;
-                if (cell[nextCell].checkForEnable(cellNumber))
-                {
-                    cell[nextCell].field[cellNumber] = (player) ? 1 : 2;
-                    nextCell = cellNumber;
-                }
-                else
-                {
-                    Console.WriteLine("Cell busy, choose another cell");
-                    return takeDataForGame(player, ref nextCell, global, cell);
-                }
-            }
-            else
-            {
-                Console.WriteLine("You input wrong value! Try again:");
-                return takeDataForGame(player, ref nextCell, global, cell);
-            }
-            return true;
-        }
-
-
-        static int takeDataForGlobalField()
-        {
-            Console.Write("Field number in Global game: ");
-            string readValue = Console.ReadLine();
-
-            if (readValue.Equals("exit"))
-            {
-                return -2;
-            }
-
-            int number;
-
-            bool result = Int32.TryParse(readValue, out number);
-            if (number >= 1 && number <= 9)
-            {
-                return --number;
-            }
-            else
-            {
-                Console.WriteLine("You input wrong value! Try again:");
-                return takeDataForGlobalField();
-            }
-        }
-
-
-        static bool checkForWin(GameField[] cell, GameField global)
-        {
-            for (int i = 0; i < 9; ++i)
-            {
-                if ((cell[i].checkForWin() == 1 || cell[i].checkForWin() == 2) && global.field[i] == 0)
-                    global.field[i] = (cell[i].checkForWin() == 1) ? 1 : 2;
-
-                if (cell[i].checkIfFull() && cell[i].checkForWin() == 0)
-                    global.field[i] = 3;
-            }
-
-            if (global.checkForWin() != 0)
-            {
-                Console.WriteLine("Player{0} - You Win!!!", global.checkForWin());
-                return false;
-            }
-
-            if(global.checkForWin() == 0 && global.checkIfFull())
-            {
-                Console.WriteLine("Draw!");
-                return false;
-            }
-            return true;
-        }
-
-
         static void showResultAtConsole(GameField[] cell, GameField global)
         {
-            global.showWinRateGameField();
+
+            showGobalField(global);
 
             string line = "";
             for (int cells = 0; cells < 3; ++cells)
@@ -196,6 +90,37 @@ namespace AlternatTicTacToe
                     line = "";
                 }
                 Console.WriteLine(" -----------------------");
+            }
+        }
+
+        static void showGobalField(GameField global)
+        {
+            string line = "|";
+            for (int i = 1; i <= 9; ++i)
+            {
+                switch (global.field[i - 1])
+                {
+                    case 0:
+                        line = String.Format("{0} _ |", line);
+                        break;
+                    case 1:
+                        line = String.Format("{0} X |", line);
+                        break;
+                    case 2:
+                        line = String.Format("{0} O |", line);
+                        break;
+                    case 3:
+                        line = String.Format("{0} * |", line);
+                        break;
+                    default:
+                        break;
+                }
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(line);
+                    line = "|";
+                }
+
             }
         }
     }
